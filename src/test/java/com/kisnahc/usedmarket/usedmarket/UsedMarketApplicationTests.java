fc1768c8575f89d0a6ad3c7a0a618c4dbf8de9b9
kisnahc.github.io <dev.kisnahc@gmail.com>
2021-08-17 11:37:38 +0900

    Initial commit

---
+++ src/test/java/com/kisnahc/usedmarket/usedmarket/UsedMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kisnahc.usedmarket.usedmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
