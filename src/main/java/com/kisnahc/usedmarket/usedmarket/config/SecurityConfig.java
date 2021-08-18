57b25c8e9b585a278ea94c973f5e8340204c6ebf
kisnahc.github.io <dev.kisnahc@gmail.com>
2021-08-18 14:26:29 +0900

    add remember me

--- src/main/java/com/kisnahc/usedmarket/usedmarket/config/SecurityConfig.java
+++ src/main/java/com/kisnahc/usedmarket/usedmarket/config/SecurityConfig.java
@@ -1,13 +1,24 @@
 package com.kisnahc.usedmarket.usedmarket.config;
 
+import com.kisnahc.usedmarket.usedmarket.domain.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final MemberService memberService;
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,6 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()
                 .loginPage("/login")
                 .permitAll();
+        http.rememberMe()
+                .userDetailsService(memberService)
+                .tokenRepository(tokenRepository());
+    }
 
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 }
